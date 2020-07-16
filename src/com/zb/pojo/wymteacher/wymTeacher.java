1ee36c88816267499d90f385f694bc6aecff711a
mobiw <mobiwusihuan288@163.com>
2020-07-16 10:01:00 +0800

    添加老师类测试

---
+++ src/com/zb/pojo/wymteacher/wymTeacher.java
@@ -0,0 +1,153 @@
+package com.zb.pojo.wymteacher;
+
+import java.util.Date;
+
+public class wymTeacher {
+
+    private Integer wymTeaId;
+    private String wymTeaJobnum;
+    private String wymTeaName;
+    private String wymTeaSex;
+    private Date wymTeaBirthday;
+    private String wymTeaIdcard;
+    private Date wymTeaStartdate;
+    private String wymTeaCollege;
+    private String wymTeaProfession;
+    private String wymTeaZhicheng;
+    private String wymTeaZhengzhimm;
+    private String wymTeaPhonenum;
+    private String wymTeaEmail;
+    private String wymTeaState;
+
+    public Integer getWymTeaId() {
+        return wymTeaId;
+    }
+
+    public void setWymTeaId(Integer wymTeaId) {
+        this.wymTeaId = wymTeaId;
+    }
+
+    public String getWymTeaJobnum() {
+        return wymTeaJobnum;
+    }
+
+    public void setWymTeaJobnum(String wymTeaJobnum) {
+        this.wymTeaJobnum = wymTeaJobnum;
+    }
+
+    public String getWymTeaName() {
+        return wymTeaName;
+    }
+
+    public void setWymTeaName(String wymTeaName) {
+        this.wymTeaName = wymTeaName;
+    }
+
+    public String getWymTeaSex() {
+        return wymTeaSex;
+    }
+
+    public void setWymTeaSex(String wymTeaSex) {
+        this.wymTeaSex = wymTeaSex;
+    }
+
+    public Date getWymTeaBirthday() {
+        return wymTeaBirthday;
+    }
+
+    public void setWymTeaBirthday(Date wymTeaBirthday) {
+        this.wymTeaBirthday = wymTeaBirthday;
+    }
+
+    public String getWymTeaIdcard() {
+        return wymTeaIdcard;
+    }
+
+    public void setWymTeaIdcard(String wymTeaIdcard) {
+        this.wymTeaIdcard = wymTeaIdcard;
+    }
+
+    public Date getWymTeaStartdate() {
+        return wymTeaStartdate;
+    }
+
+    public void setWymTeaStartdate(Date wymTeaStartdate) {
+        this.wymTeaStartdate = wymTeaStartdate;
+    }
+
+    public String getWymTeaCollege() {
+        return wymTeaCollege;
+    }
+
+    public void setWymTeaCollege(String wymTeaCollege) {
+        this.wymTeaCollege = wymTeaCollege;
+    }
+
+    public String getWymTeaProfession() {
+        return wymTeaProfession;
+    }
+
+    public void setWymTeaProfession(String wymTeaProfession) {
+        this.wymTeaProfession = wymTeaProfession;
+    }
+
+    public String getWymTeaZhicheng() {
+        return wymTeaZhicheng;
+    }
+
+    public void setWymTeaZhicheng(String wymTeaZhicheng) {
+        this.wymTeaZhicheng = wymTeaZhicheng;
+    }
+
+    public String getWymTeaZhengzhimm() {
+        return wymTeaZhengzhimm;
+    }
+
+    public void setWymTeaZhengzhimm(String wymTeaZhengzhimm) {
+        this.wymTeaZhengzhimm = wymTeaZhengzhimm;
+    }
+
+    public String getWymTeaPhonenum() {
+        return wymTeaPhonenum;
+    }
+
+    public void setWymTeaPhonenum(String wymTeaPhonenum) {
+        this.wymTeaPhonenum = wymTeaPhonenum;
+    }
+
+    public String getWymTeaEmail() {
+        return wymTeaEmail;
+    }
+
+    public void setWymTeaEmail(String wymTeaEmail) {
+        this.wymTeaEmail = wymTeaEmail;
+    }
+
+    public String getWymTeaState() {
+        return wymTeaState;
+    }
+
+    public void setWymTeaState(String wymTeaState) {
+        this.wymTeaState = wymTeaState;
+    }
+
+    @Override
+    public String toString() {
+        return "wymTeacher{" +
+                "wymTeaId=" + wymTeaId +
+                ", wymTeaJobnum='" + wymTeaJobnum + '\'' +
+                ", wymTeaName='" + wymTeaName + '\'' +
+                ", wymTeaSex='" + wymTeaSex + '\'' +
+                ", wymTeaBirthday=" + wymTeaBirthday +
+                ", wymTeaIdcard='" + wymTeaIdcard + '\'' +
+                ", wymTeaStartdate=" + wymTeaStartdate +
+                ", wymTeaCollege='" + wymTeaCollege + '\'' +
+                ", wymTeaProfession='" + wymTeaProfession + '\'' +
+                ", wymTeaZhicheng='" + wymTeaZhicheng + '\'' +
+                ", wymTeaZhengzhimm='" + wymTeaZhengzhimm + '\'' +
+                ", wymTeaPhonenum='" + wymTeaPhonenum + '\'' +
+                ", wymTeaEmail='" + wymTeaEmail + '\'' +
+                ", wymTeaState='" + wymTeaState + '\'' +
+                '}';
+    }
+}
